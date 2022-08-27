566f8e949f0463a99c4d0dc1d754690383f64115
Maithili <priyadarshinimaithili@gmail.com>
2022-08-27 15:57:36 +0530

    initial commit

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.musicplayer;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.os.Build.VERSION.SDK_INT;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -23,81 +35,162 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listview;
+    ListView listView;
     String[] items;
+    ArrayList<File> mySongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listview = findViewById(R.id.listview);
-        runtimePermission();
+        listView = (ListView) findViewById(R.id.listview);
+        //runtimePermission();
+
+        if(!checkPermission()){
+            showPermissionDialog();
+        }
+        displaySongs();
     }
 
-    private void runtimePermission() {
-        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                displaySongs();
+    private void showPermissionDialog() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setData(Uri.parse(String.format("package:%s", new Object[]{getApplicationContext().getPackageName()})));
+                startActivityForResult(intent, 2000);
+            } catch (Exception e) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                startActivityForResult(intent, 2000);
+
             }
 
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
-            }
-        }).check();
+        } else
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, 333);
     }
 
-    public ArrayList<File> findSongs(File file) {
+    private boolean checkPermission() {
+        if (SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            int write = ContextCompat.checkSelfPermission(getApplicationContext(),
+                    WRITE_EXTERNAL_STORAGE);
+            int read = ContextCompat.checkSelfPermission(getApplicationContext(),
+                    READ_EXTERNAL_STORAGE);
+
+            return write == PackageManager.PERMISSION_GRANTED &&
+                    read == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 333) {
+            if (grantResults.length > 0) {
+                boolean write = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean read = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (read && write){
+
+                }else {
+
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2000) {
+            if (SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+
+                } else {
+
+                }
+            }
+        }
+    }
+
+    /* private void runtimePermission(){
+         Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                 .withListener(new PermissionListener() {
+                     @Override
+                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                         displaySongs();
+                     }
+
+                     @Override
+                     public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                     }
+
+                     @Override
+                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                         permissionToken.continuePermissionRequest();
+                     }
+                 }).check();
+     }*/
+    public ArrayList<File> findSongs(File file){
         ArrayList<File> arrayList = new ArrayList<>();
         File[] files = file.listFiles();
-        for (File singlefile : files) {
-            if (singlefile.isDirectory() && !singlefile.isHidden()) {
+        for (File singlefile : files){
+            if(singlefile.isDirectory() && !singlefile.isHidden()){
                 arrayList.addAll(findSongs(singlefile));
-
-            } else {
-                if (singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")) {
+            }
+            else{
+                if(singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wav")){
                     arrayList.add(singlefile);
                 }
             }
         }
-        return arrayList;
+        return  arrayList;
     }
 
     public void displaySongs(){
-        final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+        //final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+
+        if(SDK_INT >= Build.VERSION_CODES.R){
+            if(Environment.isExternalStorageManager()){
+                mySongs = findSongs(new File("storage/emulated/0/Download"));
+
+            }else{
+                Log.e("msg","don't have permission");
+            }
+        }else{
+            mySongs = findSongs(Environment.getExternalStorageDirectory());
+        }
 
         items = new String[mySongs.size()];
         for(int i=0;i<mySongs.size();i++){
             items[i] = mySongs.get(i).getName().replace(".mp3","").replace(".wav","");
 
         }
-       /* ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
-        listview.setAdapter(myAdapter);*/
 
-        CustomAdapter customAdapter=new CustomAdapter();
-        listview.setAdapter(customAdapter);
+/*ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(myAdapter);*/
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        CustomAdapter customAdapter =  new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String songName=(String)listview.getItemAtPosition(i);
+                String songName = (String) listView.getItemAtPosition(i);
                 startActivity(new Intent(getApplicationContext(),PlayerActivity.class)
                         .putExtra("songs",mySongs)
-                        .putExtra("songsname",songName)
-                        .putExtra("pos",i)
-                );
+                        .putExtra("songname",songName)
+                        .putExtra("pos",i));
+
             }
         });
     }
@@ -121,13 +214,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-        View myView =getLayoutInflater()
-                .inflate(R.layout.list_item,null);
-            TextView textView=myView.findViewById(R.id.txtsongname);
-            textView.setSelected(true);
-            textView.setText(items[i]);
+            View myView = getLayoutInflater().inflate(R.layout.list_item,null);
+            TextView textsongs = myView.findViewById(R.id.txtsongname);
+            textsongs.setSelected(true);
+            textsongs.setText(items[i]);
+
             return myView;
         }
-    }
 
+    }
 }
